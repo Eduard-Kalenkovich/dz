dcc27dfeb1a2fc07a63ab3df2fb518b8ebd8798d
Eduard-Kalenkovich <125797635+Eduard-Kalenkovich@users.noreply.github.com>
2023-03-01 19:15:03 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,31 +1,16 @@
-﻿int chislo1, chislo2, chislo3;
+﻿int chislo1;
 while(true)
 {
-    Console.WriteLine("Введите первое число=");
+    Console.WriteLine("Введите число=");
         if (int.TryParse(Console.ReadLine(), out chislo1))
             break;
         Console.WriteLine("Ошибка ввода");
 }
 
-while(true)
-{
-    Console.WriteLine("Введите второе число=");
-        if (int.TryParse(Console.ReadLine(), out chislo2))
-            break;
-        Console.WriteLine("Ошибка ввода");
-}
-while(true)
-{
-    Console.WriteLine("Введите третье число=");
-        if (int.TryParse(Console.ReadLine(), out chislo3))
-            break;
-        Console.WriteLine("Ошибка ввода");
-}
 
-int max=chislo1;
-if (chislo2>max) 
-max=chislo2;
-else
-    if (chislo3>max)
-    max=chislo3;
-Console.WriteLine($"среди чисел {chislo1}, {chislo2}, {chislo3}, максимальное ->{max}");
+
+
+if (chislo1%2==0) 
+Console.WriteLine($"Число {chislo1} четное");
+else 
+Console.WriteLine($"Число {chislo1} НЕ четное"); 
