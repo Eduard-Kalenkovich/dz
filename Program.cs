a83d22d3ab95b7ccee7a8f4e71d650acadd21e3e
Eduard-Kalenkovich <125797635+Eduard-Kalenkovich@users.noreply.github.com>
2023-03-01 19:03:01 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿int chislo1, chislo2;
+while(true)
+{
+    Console.WriteLine("Введите первое число=");
+        if (int.TryParse(Console.ReadLine(), out chislo1))
+            break;
+        Console.WriteLine("Ошибка ввода");
+}
+
+while(true)
+{
+    Console.WriteLine("Введите второе число=");
+        if (int.TryParse(Console.ReadLine(), out chislo2))
+            break;
+        Console.WriteLine("Ошибка ввода");
+}
+int max=chislo1;
+if (chislo2>max) 
+Console.WriteLine($"max={chislo2}");
+else
+Console.WriteLine($"max={chislo1}");
