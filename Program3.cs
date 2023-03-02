f37d104a5f881f7f76e522844eb3d5a046143e1f
Eduard-Kalenkovich <125797635+Eduard-Kalenkovich@users.noreply.github.com>
2023-03-02 18:34:00 +0300

    Add files via upload

---
+++ Program3.cs
@@ -0,0 +1,16 @@
+﻿int chislo1;
+while(true)
+{
+    Console.WriteLine("Введите число=");
+        if (int.TryParse(Console.ReadLine(), out chislo1))
+            break;
+        Console.WriteLine("Ошибка ввода");
+}
+
+
+
+
+if (chislo1%2==0) 
+Console.WriteLine($"Число {chislo1} четное");
+else 
+Console.WriteLine($"Число {chislo1} НЕ четное"); 
